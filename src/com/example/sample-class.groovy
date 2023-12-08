cbcacf096bd1cdfcbc3767bd5ea257581600b47a
ram-repo <ramjagadeesh2205@gmail.com>
2023-12-08 21:15:50 +0530

    Create sample-class.groovy

---
+++ src/com/example/sample-class.groovy
@@ -0,0 +1,9 @@
+// src/com/example/sample-class.groovy
+
+package com.example
+
+class MySharedLibrary implements Serializable {
+    def call() {
+        echo "Hello from sample-class function!"
+    }
+}
