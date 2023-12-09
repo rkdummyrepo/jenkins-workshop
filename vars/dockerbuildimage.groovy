29648ac802780f3e1767fa0e74cc88741890ea30
ram-repo <ramjagadeesh2205@gmail.com>
2023-12-09 08:23:53 +0530

    Create dockerbuildimage.groovy

---
+++ vars/dockerbuildimage.groovy
@@ -0,0 +1,29 @@
+def call() {
+  pipeline {
+    agent any
+    environment {
+        REPO_NAME = sh(script: 'basename -s .git `git config --get remote.origin.url`', returnStdout: true).trim()
+        BUILD_NUMBER = "${env.BUILD_NUMBER}"
+        BRANCH_NAME = "${env.BRANCH_NAME}"
+    }
+      stages {
+          stage('Checkout') {
+              steps {
+                  script {
+                      // Checkout the current branch
+                      checkout scm
+                      // Print the current branch
+                  }
+              }
+          }
+        stage('docker build'){
+          steps {
+            script {
+                sh "docker image build -t spring:1.3.0 ."
+            }
+          }
+        }
+      }
+  }
+
+}
